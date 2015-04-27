01861d5bf820cdf07f4f28b92173dcb0ef999040
tewest <tewest@suffolk.edu>
2015-04-27 12:36:32 -0400

    Update Gauss_plot.py

--- Gauss_plot.py
+++ Gauss_plot.py
@@ -1,4 +1,15 @@
 # -*- coding: utf-8 -*-
+'''
+
+ Copyright (c)  2015  Thomas E. West.
+    Permission is granted to copy, distribute and/or modify this document
+    under the terms of the GNU Free Documentation License, Version 1.3
+    or any later version published by the Free Software Foundation;
+    with no Invariant Sections, no Front-Cover Texts, and no Back-Cover Texts.
+    A copy of the license is included in the section entitled "GNU
+    Free Documentation License".
+'''
+
 """
 Created on Fri Apr 24 23:57:53 2015
 
