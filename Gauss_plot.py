144247a2fa1ebe1f2234b6b81962db7369a11de4
Thomas E. West <tewest@suffolk.edu>
2015-04-27 21:05:16 -0400

    Update Gauss_plot.py

--- Gauss_plot.py
+++ Gauss_plot.py
@@ -30,7 +30,6 @@ print('''
 time.sleep(1)
 
 print('''
-    Copyright (c)  2015  Thomas E. West.
     Permission is granted to copy, distribute and/or modify this document
     under the terms of the GNU Free Documentation License, Version 1.3
     or any later version published by the Free Software Foundation;
